69b1557cdf6c64a9cfc96208f72029f299e5f2de
Andrew <andrewkok80@gmail.com>
2023-04-26 20:45:13 -0400

    Setup Next.js and add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+interface NavbarProps {
+  title: string;
+}
+
+const Navbar = ({ title }: NavbarProps) => {
+  return (
+    <div className="navbar bg-base-100 sticky top-0 z-10" data-theme="dark">
+        <div className="flex-1">
+            <Image className="ml-5"
+                src="/logo-ppsn.png"
+                alt="Logo PPSN"
+                width={48}
+                height={48}
+            />
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+                <li><a>Item 1</a></li>
+                <li tabIndex={0}>
+                    <a>
+                    Parent
+                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                    </a>
+                    <ul className="p-2 bg-base-100">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                    </ul>
+                </li>
+                <li><a>Item 3</a></li>
+            </ul>
+        </div>
+    </div>
+    );
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  return (
+    <div> 
+      <Navbar title="My Next.js App" />
+      {/* <h1> Hello, World </h1> */}
+    </div>
+  )
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
