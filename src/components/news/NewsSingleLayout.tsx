b2b971d49ad7fcfed6cabaaede2420a8abd44cf0
Andrew <andrewkok80@gmail.com>
2023-06-03 19:16:39 +0700

    use img for news single layout

--- src/components/news/NewsSingleLayout.tsx
+++ src/components/news/NewsSingleLayout.tsx
@@ -1,4 +1,19 @@
-export default function Page({ news }) {
+// import Image from "next/image";
+
+interface AuthorProps {
+    full_name: string;
+}
+
+interface NewsProps {
+    headline: string;
+    picture: string;
+    content: string;
+    pub_date: string;
+    upd_date: string;
+    authors: AuthorProps[];
+}
+
+export default function Page(news: NewsProps) {
     return <div>
         <h1> {news.headline} </h1>
         <ul>
@@ -6,6 +21,19 @@ export default function Page({ news }) {
                 <h2 key={author.full_name}> {author.full_name} </h2>
             ))}
         </ul>
+                
+        <h1> {news.picture} </h1>
+
+        <img src="http://localhost:8000/media/news-images/SAM_5130.JPG" />
+
+
+        {/* Unable to use builtin next.js image, need to configure later. Check Console for Internal Server Error */}
+        {/* <Image 
+            fill
+            src="http://localhost:8000/media/news-images/SAM_5130.JPG"
+            alt={news.headline}
+        /> */}
+
         <h2> {news.pub_date} </h2>
         <p> {news.content} </p>
     </div>
