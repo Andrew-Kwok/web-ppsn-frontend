126ed2f91384889c945d404bb75f06f7caa8d475
Andrew <andrewkok80@gmail.com>
2023-09-22 04:09:33 -0400

    add countdown

--- src/pages/oprec/pengumuman/index.tsx
+++ src/pages/oprec/pengumuman/index.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link';
 import Image from 'next/image'
 import getConfig from "next/config";
 import { useRouter } from 'next/router';
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
 
 export default function Page() {
     const { publicRuntimeConfig } = getConfig()
     const router = useRouter();
+
+    const [timeLeft, setTimeLeft] = useState(60); 
 
     const [userEmail, setUserEmail] = useState<string | null>(null);
     const [userDob, setUserDob] = useState<string | null>(null);
@@ -67,6 +69,66 @@ export default function Page() {
             setLoading(false);
         }
     } 
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const countDownDate = new Date("Oct 1, 2023 15:00:00 GMT+0700").getTime();
+            const now = new Date().getTime();
+            const distance = countDownDate - now;
+
+            if (timeLeft > 0) {
+                setTimeLeft(distance);
+            } else {
+                clearInterval(timer);
+            }
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, [timeLeft]);
+        
+
+    if (timeLeft > 0) {
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        return (
+            <div className="hero min-h-screen bg-base-200" data-theme="light">
+                <div className="hero-content text-center">
+                    <div className="max-w-2xl">
+                        <h1 className="text-5xl font-bold"> Pengumuman pada tanggal 1 Oktober 2023 Pukul 15:00 WIB </h1>
+                        <div className="grid grid-flow-col gap-5 text-center auto-cols-max mt-5 justify-center">
+                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                <span className="countdown font-mono text-5xl">
+                                    <span style={{"--value":days}}></span>
+                                </span>
+                                hari
+                            </div>
+                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                <span className="countdown font-mono text-5xl">
+                                    <span style={{"--value":hours}}></span>
+                                </span>
+                                jam
+                            </div>
+                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                <span className="countdown font-mono text-5xl">
+                                    <span style={{"--value":minutes}}></span>
+                                </span>
+                                menit
+                            </div>
+                            <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                <span className="countdown font-mono text-5xl">
+                                    <span style={{"--value":seconds}}></span>
+                                </span>
+                                detik
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )    
+    }
 
     return (
         <div data-theme="light">
